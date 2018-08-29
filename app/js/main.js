c8022b55f7a32ecef0738e3f3e27f6a94a398304
Kerthin <belichenko.roden@list.ru>
2018-08-29 03:21:03 +0300

    The creation of the script of the animation armature for smooth transition between blocks of the site

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 /********************
+	anchorAnimation.js
+********************/
+//= partials/anchorAnimation.js
+
+/********************
 	iLikeButton.js
 ********************/
 //= partials/iLikeButton.js
